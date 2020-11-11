51569f2ea4005217dc6bd39391e3d60d0280f513
erichadley8 <erichadley8@gmail.com>
2020-11-10 22:17:44 -0500

    another ad

--- public_html/categories.php
+++ public_html/categories.php
@@ -209,7 +209,24 @@ $result = $conn->query($query);
 
 
   <div class="row">
-      Horizontal Carosual Ad
+
+  <div class="col-6">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882710" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882710});</script>
+<!--JuicyAds END-->
+  </div>
+
+  <div class="col-6">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882712" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882712});</script>
+<!--JuicyAds END-->
+
+  </div>
+
     </div>
   <div class="row">
     <div class="col">
