fb7157b59b1497a112757d849d9720b0f664612b
erichadley8 <erichadley8@gmail.com>
2020-11-10 22:25:52 -0500

    added more ads

--- public_html/category.php
+++ public_html/category.php
@@ -338,8 +338,12 @@ $created = $cat[4];
     
     
     </div>
-    <div class="col">
-      Vertical Carosual Ad
+    <div class="col-2">
+      <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882717" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882717});</script>
+<!--JuicyAds END-->
     </div>
   </div>
 
