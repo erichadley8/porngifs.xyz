c4f2b77c1c6683aab5a3c1de2df83720b164d2f0
erichadley8 <erichadley8@gmail.com>
2020-11-11 15:36:37 -0500

    sitemap

---
+++ public_html/sitemap.php
@@ -0,0 +1,427 @@
+<?php
+$servername = "localhost";
+$username = "gif";
+$password = "Scbwd2blah123";
+$database = "porngifs";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D6P8CSW815"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D6P8CSW815');
+</script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>About Us | Porn Gifs: Best XXX and Blowjob Gifs></title>
+    <meta name="description" content="Porn Gifs and Blowjob Gifs About Page.  Here at Porn gifs and blowjob gifs we are all
+    about providing the best collection of porn and blowjob gifs to our visitors at the highest quality">
+  
+    <script data-ad-client="ca-pub-2176049440410237" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  </head>
+  <body>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">Top Quality Porn Gifs and Blowjob Gifs</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+          <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="categories.php">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pornstars.php">Porn Stars</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+    <div class="container-fluid">
+    
+    <div class="row">
+    
+      <div class="col-6">
+      <!-- JuicyAds v3.1 -->
+      <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+      <ins id="882686" data-width="728" data-height="102"></ins>
+      <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882686});</script>
+      <!--JuicyAds END-->
+      </div>
+
+      <div class="col-6">
+
+      <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882687" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882687});</script>
+<!--JuicyAds END-->
+    </div>
+    </div>
+    <div class="row">
+    <div class="col">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882688" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882688});</script>
+<!--JuicyAds END-->
+</div>
+<div class="col">
+     <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882689" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882689});</script>
+<!--JuicyAds END-->
+
+
+    </div>
+    <div class="col-5">
+      <div class="jumbotron">
+        <h1 class="display-4">Top Quality Porn Gif and Blowjob Gif About Us Page</h1>
+        <p class="lead">Porn Gifs and Blowjob Gifs offer you several different options to do
+          with the XXX and porn content as you so pleasure.  Please feel free to share, Download
+          and embed of porn gifs.  Please take the time to learn more about how to use Porn gif and blowjob gif website below.
+        </p>
+      </div>
+    </div>
+       
+      <div class="col">
+     <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882691" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882691});</script>
+<!--JuicyAds END-->
+
+    </div>
+    <div class="col">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882692" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882692});</script>
+<!--JuicyAds END-->
+    </div>
+    
+    </div>
+    <div class="row">
+      <div class="col-6">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882693" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882693});</script>
+<!--JuicyAds END-->
+
+      </div>
+
+      <div class="col-6">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882696" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882696});</script>
+<!--JuicyAds END-->
+      </div>
+    </div>
+    
+    <div class="row">
+    <div class="col-2">
+      <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882697" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882697});</script>
+<!--JuicyAds END-->
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882698" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882698});</script>
+<!--JuicyAds END-->
+      
+      
+          </div>
+    <div class="col-8">
+      <div class="row justify-content-center">
+        <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882707" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882707});</script>
+<!--JuicyAds END-->
+      </div>
+    <div class="container">
+        <h1>Sitemap | Porn Gifs and Blow Gifs Site</h1>
+
+        <ul class="list-group">
+            <li class="list-group-item"><a href="index.php">Home</a></li>
+            <li class="list-group-item"><a href="categories.php">Categories</a></li>
+            <li class="list-group-item"><a href="pornstars.php">Porn Stars</a></li>
+            <li class="list-group-item"><a href="about.php">About Us</a></li>
+            <li class="list-group-item"><a href="policy.php">Privacy and Policy</a></li>
+            <li class="list-group-item"><a href="terms.php">Terms of Use</a></li>
+            <li class="list-group-item"><a href="sitemap.php">Sitemap</a></li>
+        </ul>
+
+     </div>
+
+
+
+
+
+<div class="row justify-content-center">
+     <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882708" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882708});</script>
+<!--JuicyAds END-->
+      </div>
+
+    </div>
+    <div class="col-2">
+
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882705" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882705});</script>
+<!--JuicyAds END-->
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882706" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882706});</script>
+<!--JuicyAds END-->
+    </div>
+
+    
+  </div>
+
+
+  <div class="row">
+      
+<div class="col-6">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882710" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882710});</script>
+<!--JuicyAds END-->
+</div>
+
+<div class="col-6">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882712" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882712});</script>
+<!--JuicyAds END-->
+</div>
+
+    </div>
+  <div class="row">
+    <div class="col-2">
+      <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882714" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882714});</script>
+<!--JuicyAds END-->
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882716" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882716});</script>
+<!--JuicyAds END-->
+    </div>
+    <div class="col-8">
+                
+    <div class="row justify-content-center">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882724" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882724});</script>
+<!--JuicyAds END-->
+    </div>
+
+    <div class="row justify-content-center">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882726" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882726});</script>
+<!--JuicyAds END-->
+    </div>
+
+
+<div class="row justify-content-center">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882733" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882733});</script>
+<!--JuicyAds END-->
+</div>
+      
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="http://porngifs.lovestoblog.com/images/1.jpg" height="600" class="d-block w-100" alt="Blowjob gif">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>The Porn Gif and Blowjob Gifs Carousel</h5>
+            <p>
+              Cycle through Porn Gifs and Blowjob Gifs that show the best pornstars
+              doing their sexual deeds.
+            </p>
+          </div>
+        </div>
+
+
+        <?php
+          $query2 = "SELECT * FROM gifs ORDER BY id ASC LIMIT 4";
+          $result2 = $conn->query($query2);
+          while($row = $result2->fetch_assoc()){
+                    
+        ?>
+
+        <div class="carousel-item">
+          <a href="gif.php?id=<?php echo $row["id"] ?>"><img src="http://porngifs.lovestoblog.com/gifs/<?php echo $row["id"] ?>.gif" height="600" class="d-block w-100" alt="<?php echo $row["title"] ?>"></a>
+          <div class="carousel-caption d-none d-md-block">
+            <h5><?php echo $row["title"] ?></h5>
+            <p><?php echo $row["description"] ?></p>
+          </div>
+        </div>
+
+        <?php 
+          }
+        ?>
+
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    
+
+    <div class="row justify-content-center">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882729" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882729});</script>
+<!--JuicyAds END-->
+</div>
+
+<div class="row justify-content-center">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882730" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882730});</script>
+<!--JuicyAds END-->
+</div>
+
+<div class="row justify-content-center">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882732" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882732});</script>
+<!--JuicyAds END-->
+</div>
+    
+    
+    
+    </div>
+    <div class="col-2">
+      <!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882717" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882717});</script>
+<!--JuicyAds END-->
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882718" data-width="160" data-height="612"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882718});</script>
+<!--JuicyAds END-->
+    </div>
+  </div>
+
+
+
+
+  <div class="row">
+      
+          <div class="col-6">
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882720" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882720});</script>
+<!--JuicyAds END-->
+          </div>
+
+          <div class="col-6">
+
+<!-- JuicyAds v3.1 -->
+<script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+<ins id="882721" data-width="728" data-height="102"></ins>
+<script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':882721});</script>
+<!--JuicyAds END-->
+          </div>
+
+
+    </div>
+
+
+      
+
+    </div>
+
+    
+     
+    <footer>
+        <ul class="nav justify-content-center navbar-dark bg-dark">
+          <li class="nav-item">
+            <a class="nav-link" href="sitemap.php">Sitemap</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="terms.php">Terms of Use</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="policy.php">Privacy and Policy</a>
+          </li>
+        </ul>
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+   <!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5fac0d6152e70f46"></script>
+
+   
+    </body>
+</html>
+
+
+
+
+
